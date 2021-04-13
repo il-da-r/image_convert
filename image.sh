6718a1a14b71059bc2ef4fb50c04d657f91c03fc
il-da-r <42610818+il-da-r@users.noreply.github.com>
2021-04-13 22:19:13 +0300

    init commit

---
+++ image.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# картинка уменьшается до WIDTH
+WIDTH=360
+# Проверяем есть ли параметр
+if [ -z $1 ]
+    then 
+       for IMAGE in $( ls -1 | grep .jpg | grep -v thumbnail.jpg); do
+#Обрезаем расширение файла и вставляем новое имя
+       IMAGE2=$(echo $IMAGE | sed 's/\..*//')
+       IMAGE2=${IMAGE2}"_thumbnail"".jpg"
+# Проверяем есть ли уже сжатый файл    
+       if [ -f $IMAGE2 ]
+            then
+            echo "Файл $IMAGE уже имеет сжатую копию"
+#Конвертируем
+            else
+            convert -resize $WIDTH $IMAGE $IMAGE2 
+        fi    
+done
+#Если есть параметр
+    else
+#Читаем построчно файл
+        while IFS= read -r IMAGE
+        do
+#Обрезаем расширение файла и вставляем новое имя
+            IMAGE2=$(echo $IMAGE | sed 's/\..*//')
+            IMAGE2=${IMAGE}"_thumbnail"".jpg"
+# Проверяем есть ли уже сжатый файл 
+            if [ -f $IMAGE2 ]
+                then
+            echo "Файл $IMAGE уже имеет сжатую копию"
+                else
+#Конвертируем
+            convert -resize $WIDTH $IMAGE $IMAGE2   
+            fi
+        done < $1
+fi
